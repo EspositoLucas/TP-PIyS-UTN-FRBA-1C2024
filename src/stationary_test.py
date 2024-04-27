652d71071f4d6ebbd56a7c0c6831ab8b63aacf47
EspositoLucas <espositolucas2002@gmail.com>
2024-04-26 21:31:02 -0300

    agrego test de estacionaridad

---
+++ src/stationary_test.py
@@ -0,0 +1,47 @@
+import pandas as pd
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import kpss
+
+
+tesla_data = pd.read_csv('dataset/acciones_tesla_normalizados.csv')
+toyota_data = pd.read_csv('dataset/acciones_toyota_normalizados.csv')
+
+def adf_test(timeseries):
+    print("Results of Dickey-Fuller Test:")
+    dftest = adfuller(timeseries, autolag="AIC")
+    dfoutput = pd.Series(
+        dftest[0:4],
+        index=[
+            "Test Statistic",
+            "p-value",
+            "#Lags Used",
+            "Number of Observations Used",
+        ],
+    )
+    for key, value in dftest[4].items():
+        dfoutput["Critical Value (%s)" % key] = value
+    print(dfoutput)
+    
+    
+    
+
+
+def kpss_test(timeseries):
+    print("Results of KPSS Test:")
+    kpsstest = kpss(timeseries, regression="c", nlags="auto")
+    kpss_output = pd.Series(
+        kpsstest[0:3], index=["Test Statistic", "p-value", "Lags Used"]
+    )
+    for key, value in kpsstest[3].items():
+        kpss_output["Critical Value (%s)" % key] = value
+    print(kpss_output)
+
+    
+
+adf_test(toyota_data["close"])
+kpss_test(toyota_data["close"])
+
+
+    
+    
+    
