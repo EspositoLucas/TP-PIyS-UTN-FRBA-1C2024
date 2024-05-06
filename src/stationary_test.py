87fadc5b6fad32b5093886d4a79094df5c02c0f2
EspositoLucas <espositolucas2002@gmail.com>
2024-05-06 14:33:25 -0300

    agrego tets de estacionariedad

--- src/stationary_test.py
+++ src/stationary_test.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.tsa.stattools import kpss
 
 # Cargar los datasets
 tesla_data_2 = pd.read_csv('dataset/acciones_nasdaq_tesla_normalizados.csv')
@@ -22,6 +24,14 @@ plt.xlabel('Tiempo')
 plt.ylabel('Valor')
 plt.legend()
 
+plt.figure(figsize=(12, 6))
+plt.plot(toyota_data_2["close"], label='Señal original')
+plt.plot(toyota_data_2_detrended, label='Señal estacionaria')
+plt.title('Señal original y estacionaria de acciones Toyota')
+plt.xlabel('Tiempo')
+plt.ylabel('Valor')
+plt.legend()
+
 # Crear DataFrames con fechas y precios de cierre para la serie estacionaria
 tesla_data_2_detrended_df = tesla_data_2[['date', 'close']].copy()
 tesla_data_2_detrended_df['close_detrended'] = tesla_data_2_detrended
@@ -34,5 +44,30 @@ print(tesla_data_2_detrended_df.head())
 
 print("\nColumnas de fecha y precio de cierre para la serie estacionaria de Toyota:")
 print(toyota_data_2_detrended_df.head())
+
+
+# Tests de estacionariedad
+def adf_test(timeseries):
+    print("Resultados del test Dickey-Fuller aumentado (ADF):")
+    dftest = adfuller(timeseries.dropna(), autolag='AIC')
+    dfoutput = pd.Series(dftest[0:4], index=['Estadístico de prueba', 'p-valor', 'Número de rezagos usados', 'Número de observaciones usadas'])
+    for key, value in dftest[4].items():
+        dfoutput['Valor crítico (%s)' % key] = value
+    print(dfoutput)
+
+def kpss_test(timeseries):
+    print("Resultados del test KPSS:")
+    kpsstest = kpss(timeseries.dropna(), regression='c', nlags='auto')
+    kpss_output = pd.Series(kpsstest[0:3], index=['Estadístico de prueba', 'p-valor', 'Rezagos usados'])
+    for key, value in kpsstest[3].items():
+        kpss_output['Valor crítico (%s)' % key] = value
+    print(kpss_output)
+
+# Aplicar tests de estacionariedad
+adf_test(tesla_data_2_detrended)
+kpss_test(tesla_data_2_detrended)
+adf_test(toyota_data_2_detrended)
+kpss_test(toyota_data_2_detrended)
+
 
 plt.show()
